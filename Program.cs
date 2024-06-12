6380fed570833fa6da165e4127e112c18672eb59
Artur Gubaydulin <megaarcher@mail.ru>
2024-06-12 16:18:36 +0500

    making 2nd (empty) array

--- Program.cs
+++ Program.cs
@@ -7,3 +7,13 @@ for (int i = 0; i < size; i++)
 {
     arr1[i] = Console.ReadLine();
 }
+
+int size2 = 0;
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= 3)
+    {
+        size2 += 1;
+    };
+}
+String[] arr2 = new String[size2];
